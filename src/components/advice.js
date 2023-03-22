636c6205e76f9b33586a3ed86128026dd2bdb47b
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2023-03-22 14:39:03 +0000

    fetch data function running

--- src/components/advice.js
+++ src/components/advice.js
@@ -1,7 +1,14 @@
 const Advice = () => {
+    async function adviceData(event) {
+        event.preventDefault()
+        const data = await fetch(`https://api.adviceslip.com/advice`)
+            .then((response) => response.json())
+            .then((data) => data)
+            console.log(data)
+    }
     return (
         <div>
-            <button type="submit">GENERATE</button>
+            <button type="submit" onClick={event => adviceData(event)}>GENERATE</button>
         </div>
     )
 }

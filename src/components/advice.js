54e4fc2e90724795b52a7a0071b3396a4ad59416
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2023-03-22 14:20:15 +0000

    inital files set up

---
+++ src/components/advice.js
@@ -0,0 +1,9 @@
+const Advice = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Advice
---
+++ src/components/displayAdvice.js
@@ -0,0 +1,9 @@
+const DisplayAdvice = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DisplayAdvice

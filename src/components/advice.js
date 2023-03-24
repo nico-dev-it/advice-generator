3fff234c07a7235a84664c0b0989dbac8a067ab3
Nicolas Herbreteau <nicolas.herbreteau@gmail.com>
2023-03-24 18:20:14 +0000

    button styling

--- src/components/advice.js
+++ src/components/advice.js
@@ -31,6 +31,14 @@ const AdviceSlip = styled.div`
     margin: 10px;
 `
 
+const Button = styled.div`
+    position: absolute;
+    bottom: -35px;
+    border-radius: 100%;
+    padding: 15px 15px 10px;
+    background-color: #52FFA8FF;
+`
+
 const Advice = () => {
     const [advice, setAdvice] = useState([])
 
@@ -58,13 +66,14 @@ const Advice = () => {
                         </div>
                 }
                 <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-                <svg onClick={() => {
+                <Button><svg onClick={() => {
                     adviceData();
                 }} width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
                         fill="#202733"/>
                 </svg>
+                </Button>
         </StyledCard>
     )
 }

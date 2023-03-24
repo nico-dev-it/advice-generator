fda9991a2af180a90db6ccef0717f407a1dd12ce
Nicolas Herbreteau <nicolas.herbreteau@gmail.com>
2023-03-24 18:34:47 +0000

    quote styling and advice id amended

--- src/components/advice.js
+++ src/components/advice.js
@@ -15,13 +15,16 @@ const StyledCard = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-evenly;
     `
 const AdviceIdBlock = styled.div`
     color: #52FFA8FF;
     font-weight: bold;
     text-transform: uppercase;
     letter-spacing: 2px;
+    position: absolute;
+    top: 15px;
+    margin-left: 40%;
 `
 
 const AdviceSlip = styled.div`
@@ -55,25 +58,39 @@ const Advice = () => {
 
     return (
         <StyledCard>
-                {
-                    advice.data != undefined ?
+            {
+                advice.data != undefined ?
+                    <div>
+                        <DisplayAdvice data={advice.data}/>
+                    </div>
+                    : <div>
                         <div>
-                            <DisplayAdvice data={advice.data}/>
+                            <AdviceIdBlock>advice #1</AdviceIdBlock>
                         </div>
-                        : <div>
-                        <AdviceIdBlock>advice #1</AdviceIdBlock>
-                            <AdviceSlip>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</AdviceSlip>
+                        <div>
+                            <AdviceSlip>"It is easy to sit up and take notice, what's difficult is getting up and taking
+                                action."</AdviceSlip>
                         </div>
-                }
-                <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-                <Button><svg onClick={() => {
+                    </div>
+            }
+            <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd">
+                    <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
+                    <g transform="translate(212)" fill="#CEE3E9">
+                        <rect width="6" height="16" rx="3"/>
+                        <rect x="14" width="6" height="16" rx="3"/>
+                    </g>
+                </g>
+            </svg>
+            <Button>
+                <svg onClick={() => {
                     adviceData();
                 }} width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
                         fill="#202733"/>
                 </svg>
-                </Button>
+            </Button>
         </StyledCard>
     )
 }
--- src/components/displayAdvice.js
+++ src/components/displayAdvice.js
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+
 const DisplayAdvice = (props) => {
+    const AdviceIdBlock = styled.div`
+    color: #52FFA8FF;
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    position: absolute;
+    top: 15px;
+    margin-left: 40%;
+`
+    const AdviceSlip = styled.div`
+    color: #CEE3E9FF;
+    font-size: 1.8rem;
+    font-weight: bold;
+    margin: 10px;
+`
     console.log(props)
     return (
         <div>
-            <h4>advice #{props.data.slip.id}</h4>
-            <h3>"{props.data.slip.advice}"</h3>
+            <div>
+                <AdviceIdBlock>advice #{props.data.slip.id}</AdviceIdBlock>
+            </div>
+            <div>
+                <AdviceSlip>"{props.data.slip.advice}"</AdviceSlip>
+            </div>
         </div>
     )
 }

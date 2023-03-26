a8d1e29d19ef23f92cd0645c168b8068e1813298
Nicolas Herbreteau <nicolas.herbreteau@gmail.com>
2023-03-26 12:51:46 +0100

    updated mobile sizing and mobile divider svg added and styled

--- src/components/advice.js
+++ src/components/advice.js
@@ -17,7 +17,6 @@ const StyledCard = styled.div`
     @media (max-width: 670px) {
     width: 370px;
     height: 350px;
-    margin-left: 2em;
   }
     `
 const AdviceIdBlock = styled.div`
@@ -44,8 +43,19 @@ const AdviceSlip = styled.div`
 const Divider = styled.div `
     position: relative;
     bottom: 30px;
+    @media (max-width: 670px) {
+    display: none;
+    }
 `
 
+const DividerMobile = styled.div `
+    display: none;
+    @media (max-width: 670px) {
+    display: flex;
+    position: relative;
+    top: -20px;
+    }
+`
 const Button = styled.div`
     position: absolute;
     bottom: -35px;
@@ -100,6 +110,9 @@ const Advice = () => {
                 </g>
             </svg>
             </Divider>
+            <DividerMobile>
+                <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+            </DividerMobile>
             <Button>
                 <div>
                 <svg onClick={() => {
